42b0ac6e6b050262de48e823d60575dc9e093339
bhandari_shubham <shubhambhandari12345hb@gmail.com>
2024-08-12 20:37:20 +0530

    change vite to cra

--- src/components/share/Modal.tsx
+++ src/components/share/Modal.tsx
@@ -1,5 +1,4 @@
-import { Dispatch, useEffect, useRef } from "react";
-import { createPortal } from "react-dom";
+import { useEffect, useRef } from "react";
 
 export function Modal({ modalOpen, setModalOpen, children }: { modalOpen: boolean; setModalOpen: () => void; children: React.ReactNode }) {
   const dialogRef = useRef(null);
@@ -11,13 +10,12 @@ export function Modal({ modalOpen, setModalOpen, children }: { modalOpen: boolea
       document.body.style.overflow = "auto";
     };
   }, [modalOpen]);
-  return createPortal(
+  return (
     <div className={`${modalOpen ? "modal-backdrop" : ""}`}>
       <dialog ref={dialogRef} open={modalOpen} className={`${modalOpen ? "modal" : ""}`} onClose={setModalOpen}>
         {children}
         <button onClick={setModalOpen} className="close-button" />
       </dialog>
-    </div>,
-    document.getElementById("modal-root")!
+    </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
+import { store } from "./redux/store";
 
 const queryClient = new QueryClient();
 
